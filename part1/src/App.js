3f79c08c5b59d76da27fbf3a82d09b4e199ecd72
ksajan <ksajan12@gmail.com>
2022-01-23 23:36:16 +0530

    completed exercise 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -35,12 +35,16 @@ const App2d = () => {
 const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad;
   const average = (props.good - props.bad) / total;
+  const positive = props.good / total * 100;
 
   return (
     <div>
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
