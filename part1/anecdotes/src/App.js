4c408aa7fe6e64ba3586b4cf1c662ecb14b097be
ksajan <ksajan12@gmail.com>
2022-01-24 01:54:08 +0530

    anecdotes exercise partial submission

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const GetRandomInt = ({max}) => {
+  console.log(max)
+  return Math.floor(Math.random() * (max))
+}
+
+
+const Button = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>
+      {props.text}
     </div>
-  );
+  )
+}
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const [selected, setSelected] = useState(0)
+  
+  const handleClick = () => {
+    // console.log(anecdotes.length)
+    let index = GetRandomInt(anecdotes.length)
+    while (index === selected) {
+      index = GetRandomInt(anecdotes.length)
+    }
+    setSelected(index)
+  }
+  console.log(GetRandomInt(0, anecdotes.length))
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <Display text={anecdotes[selected]} />
+      <Button handleClick={handleClick} text={'Next anecdote'} />
+      {/* {anecdotes[selected]} */}
+    </div>
+  )
 }
 
 export default App;
