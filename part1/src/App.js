6d12d648de5215601465865b062a115203d7e1c8
ksajan <ksajan12@gmail.com>
2022-01-23 23:34:40 +0530

    completed exercise 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,6 +2,48 @@
 import React, { useState } from 'react';
 
 
+
+const App2d = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <NewButton handleClick={handleGood} text="good" />
+      <NewButton handleClick={handleNeutral} text="neutral" />
+      <NewButton handleClick={handleBad} text="bad" />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad;
+  const average = (props.good - props.bad) / total;
+
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  )
+}
 const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
@@ -228,4 +270,4 @@ const Total = (props) => {
 }
 
 
-export default App2;
+export default App2d;
