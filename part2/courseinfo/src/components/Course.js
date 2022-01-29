b3ab4713ec91ecba76b27ae9c7de2c12d34a1d5d
ksajan <ksajan12@gmail.com>
2022-01-29 16:19:45 +0530

    completed exercise 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,17 +1,5 @@
 import React from 'react'
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
-
-const IndividualCourse = ({course}) => {
-    return (
-        <div>
-            <Header course={course} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
+import IndividualCourse from './IndivisualCourse'
 
 const Course = ({ course }) => {
     return (
