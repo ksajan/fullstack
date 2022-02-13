d87b39ea8a7b3488c1b874e47a5445d7750402bd
ksajan <ksajan12@gmail.com>
2022-02-13 18:11:37 +0530

    completed exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,46 @@
 import React, { useState } from 'react'
+import Note from './components/Note'
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [showName, setShowName] = useState(false)
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    console.log('button clicked', event.target)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const namesToShow = showName
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes('a'))
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+        <Note key={person.name} note={person} />)}
+      </ul>
     </div>
   )
 }
