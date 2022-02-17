051fa5a7f6b28287ee3c5eb16467e38dd9ba7a7a
ksajan <ksajan12@gmail.com>
2022-02-17 12:17:21 +0530

    completed exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
-  const [showName, setShowName] = useState(false)
+  const [message, setMessage] = useState(null)
+
+  // const names = persons.map(person => {
+  //   return person.name.toLocaleLowerCase()
+  // })
 
   const addPerson = (event) => {
     event.preventDefault()
+    const matchNames = (person1, person2) => person1.toLocaleLowerCase() === person2.toLocaleLowerCase()
+    if (persons.find((person) => matchNames(person.name, newName))){
+      window.alert(`${newName} already exists in the phonebook.`)
+      return
+    }else {
+      const replacer = (match, p1, p2) => {
+        return p1.toUpperCase() + p2.toLocaleLowerCase();
+      }
+
+    const titleCasedName = newName.replace(/\b([a-zA-Z])(\w+)/g, replacer)
     const personObject = {
-      name: newName,
+      name: titleCasedName,
     }
     setPersons(persons.concat(personObject))
+  }
     setNewName('')
     console.log('button clicked', event.target)
   }
@@ -22,9 +38,9 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-  const namesToShow = showName
-  ? persons
-  : persons.filter(person => person.name.toLowerCase().includes('a'))
+  // const namesToShow = showName
+  // ? persons
+  // : persons.filter(person => person.name.toLowerCase().includes('a'))
   return (
     <div>
       <h2>Phonebook</h2>
