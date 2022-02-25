476b0dd81f76dd70f032d5357eeb52524f0ee5ed
ksajan <ksajan12@gmail.com>
2022-02-26 04:40:27 +0530

    completed exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   // const [message, setMessage] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -19,6 +15,15 @@ const App = () => {
 
   // const filterByName = persons.filter(person => {
   //   return person.name.toLocaleLowerCase().includes(nameFilter.toLocaleLowerCase())}) 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
