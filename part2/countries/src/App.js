a2f9e479248ac7c609c6dcf1e274fef75e39df03
ksajan <ksajan12@gmail.com>
2022-02-26 05:01:05 +0530

    exercise 2.12 base code completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    const url = 'https://restcountries.com/v3.1/all'
+    axios
+      .get(url)
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const countriesToShow = countries.filter(country =>
+    country.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Countries</h2>
+      <div>
+        find countries: <input value={filter} onChange={handleFilterChange} />  
+
+        <button onClick={() => setFilter('')}>show all</button>
+        
+        <h3>{countriesToShow.length} countries</h3>
+        <ul>
+          {countriesToShow.map(country =>
+            <li key={country.name}>{country.name}</li>
+          )}
+        </ul>
+        
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
