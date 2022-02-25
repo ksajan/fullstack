da36b063355289fbb4b97d3b78b0f34c14a42556
ksajan <ksajan12@gmail.com>
2022-02-26 04:27:16 +0530

    completed learning exercise of part2 c

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,11 +1,19 @@
-import React, {useState} from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [ newNote, setNewNote ] = useState('a new note')
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
@@ -15,36 +23,39 @@ const App = (props) => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
-  // }
-    console.log('button clicked', event.target)
   }
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
   const notesToShow = showAll
-  ? notes 
-  : notes.filter(note => note.important === true)
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
-
-        <button onClick={() => setShowAll(!showAll)}>Show {showAll ? 'important' : 'all'} </button>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
       </div>
       <ul>
-        {/* {notes.map(note => 
-            <Note key={note.id} note={note} />
-        )} */}
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />)}
+          <Note key={note.id} note={note} />
+        )}
       </ul>
       <form onSubmit={addNote}>
-          <input value={newNote} onChange={handleNoteChange}/>
-          <button type="submit">save</button>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
       </form>
     </div>
   )
--- learning/src/index.js
+++ learning/src/index.js
@@ -1,17 +1,10 @@
-import ReactDOM from 'react-dom'
-import App from './App.js'
-import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// const promise = axios.get('http://localhost:3001/notes')
-
-// promise.then(response => {
-//   console.log(response)
-//   ReactDOM.render(<App notes={response.data} />, document.getElementById('root'))
-// })
-
-// const promise2 = axios.get('http://localhost:3001/foobar')
-// console.log(promise2)
-axios.get('http://localhost:3001/notes').then(response => {
-  console.log(response)
-  ReactDOM.render(<App notes={response.data} />, document.getElementById('root'))
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
